cabd34627365fa526f89b2a9e2eeb38027efb7b0
Ankit Baraskar <ankit.baraskar@atidiv.com>
2017-08-23 11:10:58 +0530

    first code

---
+++ peak_finder_uni.py
@@ -0,0 +1,27 @@
+seqfile_name = "seq.txt"
+
+
+def loadfile():
+    print("Loading the sequence from file")
+    with open(seqfile_name) as sf:
+        array = []
+        for line in sf:
+            array += ([float(x) for x in line.split()])
+    return array
+
+def findpeak(seq, startind, finind, n):
+    mid = (startind + finind)//2
+    print(startind, mid, finind)
+    if (mid == 0) | ((seq[mid-1] <= seq[mid]) & (seq[mid] >= seq[mid+1])) | (mid == n):
+        return mid
+    elif (mid > 0) & (seq[mid-1] >= seq[mid]):
+        return findpeak(seq, startind, mid, n)
+    else:
+        return findpeak(seq, mid+1, finind, n)
+
+seq_test = loadfile()
+
+n = len(seq_test)
+print(n)
+print(seq_test)
+print(findpeak(seq_test, 0, n-1, n))
